27159782d67067306ee38b482a43fbaa4c208029
baum456 <mingqi_li@icloud.com>
2024-07-25 22:08:51 +0200

    Initial Commit

---
+++ TheHuntingGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheHuntingGame
+//
+//  Created by Mingqi Li on 25.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheHuntingGame/TheHuntingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheHuntingGameApp.swift
+//  TheHuntingGame
+//
+//  Created by Mingqi Li on 25.07.24.
+//
+
+import SwiftUI
+
+@main
+struct TheHuntingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
